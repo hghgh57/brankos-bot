619c8065b05a10842a4b549e7913d272d8ab615a
hghgh57 <rizzbut3@gmail.com>
2026-09-07 08:36:53 +1000

    Update guildMemberAdd.js

--- discord-application-bot-full/events/guildMemberAdd.js
+++ discord-application-bot-full/events/guildMemberAdd.js
@@ -1,4 +1,3 @@
-const { EmbedBuilder } = require("discord.js");
 const config = require("../config");
 
 module.exports = {
@@ -7,14 +6,14 @@ module.exports = {
   async execute(member) {
     if (!config.welcomeChannelId) return;
 
-    const channel = member.guild.channels.cache.get(config.welcomeChannelId);
+    const channel = member.guild.channels.cache.get(
+      config.welcomeChannelId
+    );
+
     if (!channel) return;
 
-    const embed = new EmbedBuilder()
-      .setDescription(`Hey ${member}, **Welcome To ${member.guild.name}!** 🎉`)
-      .setColor(0x5865F2)
-      .setTimestamp();
-
-    await channel.send({ embeds: [embed] }).catch(console.error);
+    await channel.send(
+      `Hey ${member}, Welcome To ${member.guild.name}! 🎉`
+    ).catch(console.error);
   }
 };
