9c82b753df847b203bce718df46e5dd3d7107add
hghgh57 <rizzbut3@gmail.com>
2026-09-07 07:57:40 +1000

    Update channel IDs in config.js

--- discord-application-bot-full/config.js
+++ discord-application-bot-full/config.js
@@ -3,13 +3,13 @@ module.exports = {
   // CHANNEL IDS
   // ==================================================
 
-  welcomeChannelId: "PUT_WELCOME_CHANNEL_ID_HERE",
+  welcomeChannelId: "1438877108452593765",
 
-  leaveChannelId: "PUT_LEAVE_CHANNEL_ID_HERE",
+  leaveChannelId: "1438877108452593765",
 
-  applicationChannelId: "PUT_APPLICATION_CHANNEL_ID_HERE",
+  applicationChannelId: "1492509675520790598",
 
-  transcriptChannelId: "PUT_TRANSCRIPT_CHANNEL_ID_HERE",
+  transcriptChannelId: "1499507598691536896",
 
 
   // ==================================================
