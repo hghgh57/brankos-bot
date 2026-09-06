35e021eb48a156e9710de6270aa8ac9e99e22bbc
hghgh57 <rizzbut3@gmail.com>
2026-09-07 07:02:24 +1000

    Update config.js

--- discord-application-bot-full/config.js
+++ discord-application-bot-full/config.js
@@ -16,10 +16,46 @@ module.exports = {
   // PAID AD
   // ==============================
 
-  paidAd: {
-    title: "PAID AD",
-    message: "Your paid advertisement message goes here."
-  },
+ paidAd: {
+  title: "💰 PAID ADVERTISEMENTS",
+  message: `#  ** :coins:   __PAID ADVERTISMENTS__    **  
+ 
+ 
+## :Pings:  •  Ping Options: 
+ 
+### Weekday Prices : 
+- ** @here = \`1,00€\`** 
+- ** <@&1438912689186406411>  = \`2,50€\`** 
+- ** @everyone = \`3,00€\`** 
+### Weekend Prices : 
+- ** @here = \`2,00€\`** 
+- ** <@&1438912689186406411>   = \`3,50€\` (everyone except famous people) ** 
+- ** @everyone =  \`4,00€\` (pings famous people like <@884056543576866886> , <@988883385332170872> , <@944232264856928256> , <@1389443504673128448> ) ** 
+ 
+## :giftbox:  •  Addons (Recommended)  
+- ** :6635paymentnitroclassic: Nitro Basic: \`4€\`** 
+- ** :126620nitro: Nitro Premium \`11€\`** 
+> -# More members will join for a chance to win Nitro 
+ 
+## :hashtag:  •  Custom Channel  
+- **Channel for 1 day: \`1€\`** 
+- **Channel for 3 days: \`2,5€\`** 
+- **Channel for 7 days: \`5€\`** 
+> -# for custom amount of days it is \`1€\` per day 
+ 
+ 
+## **:Cash:  •  __Payment Methods __** 
+ 
+> - **PayPal :Paypal:   ** 
+> - **Crypto :Crypto:    ** 
+** All Fees must be covered by you** 
+ 
+> #  :Rules: Rules : 
+> - **No Refunds** 
+> - If your server breaks **Donut Rules or Discord Terms of Service** , your ad will be removed (or won't be posted). 
+> - Once your ad is approved and posted, you cannot change the **text, ping, or the ad without prior approval.** 
+> - **Payment** goes through <@1253357195479748678>  **__ONLY__**`
+},
 
   // ==============================
   // APPLICATIONS
