59b78ed5bd226d1f3f42e9a294cca2d5ecf92564
hghgh57 <rizzbut3@gmail.com>
2026-09-07 07:31:05 +1000

    Update deploy-commands.js

--- discord-application-bot-full/deploy-commands.js
+++ discord-application-bot-full/deploy-commands.js
@@ -5,22 +5,33 @@ const fs = require("fs");
 const path = require("path");
 
 const commands = [];
+
 const commandsPath = path.join(__dirname, "commands");
 
-for (const file of fs.readdirSync(commandsPath).filter(f => f.endsWith(".js"))) {
+for (
+  const file of fs.readdirSync(commandsPath).filter(f => f.endsWith(".js"))
+) {
   const command = require(path.join(commandsPath, file));
+
   commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+const rest = new REST({
+  version: "10"
+}).setToken(process.env.TOKEN);
 
 (async () => {
   try {
     console.log(`Registering ${commands.length} slash commands...`);
 
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-      { body: commands }
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      {
+        body: commands
+      }
     );
 
     console.log("Slash commands registered.");
