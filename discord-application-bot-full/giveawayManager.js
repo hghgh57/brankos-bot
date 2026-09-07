5d9acff1ba4ef3a51b2c7f1c1c4df26d4401a560
hghgh57 <rizzbut3@gmail.com>
2026-09-08 01:02:01 +1000

    Update giveawayManager.js

--- discord-application-bot-full/giveawayManager.js
+++ discord-application-bot-full/giveawayManager.js
@@ -84,7 +84,9 @@ function createGiveawayEmbed(giveaway) {
       ? "🎉 This giveaway has ended!"
       : "Click the button below to enter!",
     "",
-    `**Winners:** ${giveaway.winnerCount}`,
+    hasWinners
+      ? `**Winner(s):** ${giveaway.winners.map(id => `<@${id}>`).join(" ")}`
+      : `**Winners:** ${giveaway.winnerCount}`,
     `**Hosted by:** ${giveaway.host}`,
     hasWinners
       ? `**Ends:** <t:${endTimestamp}:R>`
@@ -92,13 +94,6 @@ function createGiveawayEmbed(giveaway) {
     "",
     `<t:${endTimestamp}:F>`
   ];
-
-  if (hasWinners) {
-    lines.push(
-      "",
-      `**Winner(s):** ${giveaway.winners.map(id => `<@${id}>`).join(" ")}`
-    );
-  }
 
   return new EmbedBuilder()
     .setColor(0x0000ff)
