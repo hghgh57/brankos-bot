fba3958c37b869ceee9f68d5d8593847c08e66bf
hghgh57 <rizzbut3@gmail.com>
2026-09-07 15:46:34 +1000

    Refactor command and event loading for clarity

--- discord-application-bot-full/index.js
+++ discord-application-bot-full/index.js
@@ -23,17 +23,53 @@ const client = new Client({
 
 client.commands = new Collection();
 
+// Make the client available to other files
+global.client = client;
+
+// Load commands
 const commandsPath = path.join(__dirname, "commands");
-for (const file of fs.readdirSync(commandsPath).filter(f => f.endsWith(".js"))) {
-  const command = require(path.join(commandsPath, file));
-  client.commands.set(command.data.name, command);
+
+for (
+  const file of fs
+    .readdirSync(commandsPath)
+    .filter(file => file.endsWith(".js"))
+) {
+  const command = require(
+    path.join(commandsPath, file)
+  );
+
+  client.commands.set(
+    command.data.name,
+    command
+  );
 }
 
+// Load events
 const eventsPath = path.join(__dirname, "events");
-for (const file of fs.readdirSync(eventsPath).filter(f => f.endsWith(".js"))) {
-  const event = require(path.join(eventsPath, file));
-  if (event.once) client.once(event.name, (...args) => event.execute(...args, client));
-  else client.on(event.name, (...args) => event.execute(...args, client));
+
+for (
+  const file of fs
+    .readdirSync(eventsPath)
+    .filter(file => file.endsWith(".js"))
+) {
+  const event = require(
+    path.join(eventsPath, file)
+  );
+
+  if (event.once) {
+    client.once(
+      event.name,
+      (...args) =>
+        event.execute(...args, client)
+    );
+  } else {
+    client.on(
+      event.name,
+      (...args) =>
+        event.execute(...args, client)
+    );
+  }
 }
 
+// Login
 client.login(process.env.TOKEN);
