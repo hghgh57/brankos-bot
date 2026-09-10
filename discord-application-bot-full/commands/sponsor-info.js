e45b418f5e000b5c9e1f299272a8667a4d950f5e
hghgh57 <rizzbut3@gmail.com>
2026-09-10 22:25:45 +1000

    Update sponsor-info.js

--- discord-application-bot-full/commands/sponsor-info.js
+++ discord-application-bot-full/commands/sponsor-info.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -7,22 +7,30 @@ module.exports = {
     .setDefaultMemberPermissions(null),
 
   async execute(interaction) {
-    const content =
-      "**Giveaway Sponsor Tiers**\n\n" +
-      "**1M-10M**: <@&1504931998798446764> \n" +
-      "**10M-25M**: <@&1515770869517914243> \n" +
-      "**25M-50M**: <@&1515770944252149830> \n" +
-      "**50M-75M**: <@&1515771316307755088>\n" +
-      "**75M-100M**: <@&1515771454312939721> \n\n" +
-      "**100M-250M**: <@&1515772812466458675> \n" +
-      "**250M-500M**: <@&1515773022357819463> \n" +
-      "**500M-1B**: <@&1515774272986026165> \n" +
-      "**1B+**: <@&1515774819864547582> \n" +
-      "**Highest sponsor : <@&1547581798177833012>  **";
+    const embed = new EmbedBuilder()
+      .setColor(0x0000ff)
+      .setTitle("Giveaway Sponsor Tiers")
+      .setDescription(
+        "**1M-10M**: <@&1504931998798446764> \n" +
+        "**10M-25M**: <@&1515770869517914243> \n" +
+        "**25M-50M**: <@&1515770944252149830> \n" +
+        "**50M-75M**: <@&1515771316307755088>\n" +
+        "**75M-100M**: <@&1515771454312939721> \n\n" +
+        "**100M-250M**: <@&1515772812466458675> \n" +
+        "**250M-500M**: <@&1515773022357819463> \n" +
+        "**500M-1B**: <@&1515774272986026165> \n" +
+        "**1B+**: <@&1515774819864547582> \n" +
+        "**Highest sponsor : <@&1547581798177833012>  **"
+      );
+
+    await interaction.channel.send({
+      embeds: [embed],
+      allowedMentions: { roles: [] } // remove this line if you want the roles to actually ping
+    });
 
     await interaction.reply({
-      content,
-      allowedMentions: { roles: [] } // remove this line if you want the roles to actually ping
+      content: "✅ Sponsor info sent!",
+      ephemeral: true
     });
   }
 };
