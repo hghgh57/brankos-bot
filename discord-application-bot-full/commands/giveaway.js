a81e244c171a1233cc393f40fb809483c8bdaeee
hghgh57 <rizzbut3@gmail.com>
2026-09-12 22:16:38 +1000

    Update giveaway.js

--- discord-application-bot-full/commands/giveaway.js
+++ discord-application-bot-full/commands/giveaway.js
@@ -4,12 +4,13 @@ const {
 } = require("discord.js");
 
 const { startGiveaway } = require("../giveawayManager");
+const config = require("../config");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("gcreate")
     .setDescription("Start a giveaway.")
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+    .setDefaultMemberPermissions(null)
     .addStringOption(option =>
       option
         .setName("prize")
@@ -41,9 +42,12 @@ module.exports = {
       });
     }
 
-    if (!interaction.memberPermissions?.has(PermissionFlagsBits.ManageGuild)) {
+    const hasPermission = interaction.memberPermissions?.has(PermissionFlagsBits.ManageGuild);
+    const hasStaffRole = interaction.member.roles.cache.has(config.staffRoleId);
+
+    if (!hasPermission && !hasStaffRole) {
       return interaction.reply({
-        content: "❌ You need the Manage Server permission to use this command.",
+        content: "❌ You need the Manage Server permission (or the staff role) to use this command.",
         ephemeral: true
       });
     }
